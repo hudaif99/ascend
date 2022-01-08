d7663e4bf9a6cdc842b3d7b36aadf15f0eca95b6
hudaif99 <hudaifpaladan99@gmail.com>
2022-01-08 10:57:28 +0530

    email and password fields created

--- lib/login.dart
+++ lib/login.dart
@@ -13,20 +13,27 @@ class _LoginPageState extends State<LoginPage> {
     return SafeArea(
       child: Scaffold(
         body: Container(
-          padding: EdgeInsets.only(top: 50,left: 25),
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
+          padding: EdgeInsets.only(top: 50, left: 25),
           color: Colors.green,
           child: Column(
             children: [
-              Column(
+              Row(
                 children: [
-                  Row(
-                    children: [
-                      Icon(Icons.arrow_back,color: Colors.white,),
-                      SizedBox(
-                        width: 30,
-                      ),
-                      Text("Login", style: TextStyle(fontSize: 30,fontWeight: FontWeight.normal,color: Colors.white),)
-                    ],
+                  Icon(
+                    Icons.arrow_back,
+                    color: Colors.white,
+                  ),
+                  SizedBox(
+                    width: 30,
+                  ),
+                  Text(
+                    "Login",
+                    style: TextStyle(
+                        fontSize: 30,
+                        fontWeight: FontWeight.normal,
+                        color: Colors.white),
                   )
                 ],
               ),
@@ -37,12 +44,64 @@ class _LoginPageState extends State<LoginPage> {
                 padding: EdgeInsets.all(12),
                 child: Column(
                   children: [
-                    Row(
+                    Column(
                       children: [
-                        Text("Email or Mobile",style: TextStyle(fontSize: 15,fontWeight: FontWeight.bold,color: Colors.white),),
-
+                        Row(
+                          children: [
+                            Text(
+                              "Email or Mobile",
+                              style: TextStyle(
+                                  fontSize: 17,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.white),
+                            ),
+                          ],
+                        ),
                       ],
                     ),
+                    Column(
+                      children: [
+                        TextField(
+                          decoration: InputDecoration(
+                              border: UnderlineInputBorder(
+                                  borderSide:
+                                      new BorderSide(color: Colors.white)),
+                              hintText: 'your@email.com',
+                              hintStyle: TextStyle(color: Colors.white)),
+                        )
+                      ],
+                    ),
+                    Container(
+                      padding: EdgeInsets.only(top: 17,bottom: 12),
+                      child: Column(
+                        children: [
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                           mainAxisAlignment: MainAxisAlignment.start,
+                            children: [
+                              Text("Password",
+                                  style: TextStyle(
+                              fontSize: 17,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white),)
+                            ],
+                          ),
+                          Column(
+                            children: [
+                              TextField(
+                                decoration: InputDecoration(
+                                    border: UnderlineInputBorder(
+                                        borderSide:
+                                        new BorderSide(color: Colors.white)),
+                                    hintText: 'Enter Password',
+                                    hintStyle: TextStyle(color: Colors.white)),
+                                obscureText: true,
+                              )
+                            ],
+                          )
+                        ],
+                      ),
+                    )
                   ],
                 ),
               )
